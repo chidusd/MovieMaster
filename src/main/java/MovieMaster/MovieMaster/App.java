44a100c1e8a8c36ad45bcd6c3ecfa09484f78054
swap <swap@192.168.1.9>
2017-06-10 12:00:56 +0530

    Created folder structure.

---
+++ src/main/java/MovieMaster/MovieMaster/App.java
@@ -0,0 +1,13 @@
+package MovieMaster.MovieMaster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
